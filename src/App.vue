dfab0c8fcf42a381a91595a162e7419fb2924ae1
Rodrigo <snowitxc@gmail.com>
2023-01-24 20:35:48 -0300

    fix

--- src/App.vue
+++ src/App.vue
@@ -34,10 +34,12 @@ onBeforeMount(async()=>{
   contractAddress.value = LotteryContract.options.address;
   managerAddres.value = await LotteryContract.methods.manager().call();
   prizePool.value = await web3.eth.getBalance(contractAddress.value);
-  players.value = await LotteryContract.methods.getPlayers().call();
-  lastWinner.value = await LotteryContract.methods.lastWinner().call();
+  const playersArr = await LotteryContract.methods.getPlayers().call();
 
-  
+  playersArr.map(item =>{
+    players.value.push(item);
+  })
+  lastWinner.value = await LotteryContract.methods.lastWinner().call();
   accounts.value = await web3.eth.getAccounts();
   loading.value = false;
    
@@ -57,7 +59,7 @@ async function pickAWinner(){
       await LotteryContract.methods.pickWinner().send({
         from: accounts.value[0],
       });
-      players.value = await LotteryContract.methods.lastWinner().call();  //update lastWinner
+      lastWinner.value = await LotteryContract.methods.lastWinner().call();  //update lastWinner
       prizePool.value = 0;
       onPickingAWinner.value = false;
       players.value = [];
@@ -78,6 +80,7 @@ async function enter(){
       from: accounts.value[0],
       value: web3.utils.toWei(prizeEnter.value,"ether")
     });
+    players.value.push(accounts.value[0]);
     prizePool.value = parseFloat(prizePool.value) + parseFloat(web3.utils.toWei(prizeEnter.value,"ether"))
     onEnter.value = false;
     swal.fire(
@@ -97,9 +100,9 @@ async function enter(){
 <template>
   <LoadingPage v-if="loading"></LoadingPage>
 
-  <div class="w-full  bg-gray-100 absolute " v-if="!loading">
+  <div class="w-full  bg-gray-100 absolute min-h-screen" v-if="!loading">
     <div class="flex flex-col justify-center w-full  items-center mt-10 px-10">
-      <button class="bg-violet-700 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded  md:py-8 text-3xl animate-bounce hover:animate-pulse" @click="enter">
+      <button class="bg-violet-700 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded  md:py-8 text-3xl animate-bounce hover:animate-pulse" @click="enter" :disabled="onEnter || onPickingAWinner">
        <span v-if="!onEnter"> Enter to the Lottery (<span class="text-green-500">{{ getPrizePoolEth }} ETH in Cash  </span>)
         prize:  0.01 ETH</span>
         <span v-if="onEnter"> 
@@ -114,34 +117,36 @@ async function enter(){
         <i class="fa-duotone fa-face-tongue-money"></i>
       </button>
 
-      <button v-if="userIsManager && players.length > 0" class="rounded-full mt-3 bg-red-800 hover:bg-violet-700 text-white font-bold py-2 px-4 rounded md:py-5 py-4  text-3xl  hover:animate-pulse" @click="pickAWinner">
-          Pick a Winner
+      <button v-if="userIsManager && players.length > 0" class="md:w-1/4 sm:w-3/4 rounded-full mt-3 bg-red-800 hover:bg-violet-700 text-white font-bold py-2 px-4 rounded md:py-5 py-4  text-3xl  hover:animate-pulse flex justify-center gap-3" @click="pickAWinner" :disabled="onPickingAWinner  || onEnter">
+           <span>Pick a Winner</span>
           <div role="status" v-if="onPickingAWinner">
             <svg aria-hidden="true" class="inline w-10 h-10 mr-2 text-gray-200 animate-spin dark:text-gray-600 fill-blue-600" viewBox="0 0 100 101" fill="none" xmlns="http://www.w3.org/2000/svg">
                 <path d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z" fill="currentColor"/>
                 <path d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z" fill="currentFill"/>
             </svg>
             <span class="sr-only">Loading...</span>
-        </div>
+          </div>
         </button>
     </div>
     
   
     <div class="flex flex-wrap px-5 mt-10 gap-10">
-      <div class="flex-1">
+      <div class="flex-1 max-w-sm lg:max-w-xl">
         <div class="bg-white px-5 py-5 rounded">
-          <span class="text-3xl font-medium dark:text-white">Lottery Contract</span>
+          <span class="text-3xl font-medium ">Lottery Contract</span>
           <p class="mb-3 font-light text-gray-900 text-xl mt-3 dark:text-gray-400">This is a smart contract
             that simulates a lottery
             created and implemented in the Rinkeby testnet created by Rodrigo Castro. To use this application the user must have a metamask account with fictitious money loaded in the Rinkeby network, the cost of the ticket is 0.01 ETH
           </p>
-    
-          <ul class="mt-5 list-disc ml-5">
-            <li class="text-gray-900 mb-2"><span class="font-medium">Contract address: </span>{{ contractAddress }}</li>
-            <li class="text-gray-900 mb-2"><span class="font-medium">Last Winner: </span> {{ lastWinner ? lastWinner : 'Nothing' }}</li>
-            <li class="text-gray-900 mb-2"><span class="font-medium">Manager Address: </span> {{ managerAddres }}</li>
-            <li class="text-gray-900 mb-2"><span class="font-medium">Cantidad de Jugadores: {{ cantPlayers }} </span> </li>
-          </ul>
+
+          <div class="w-full bg-white rounded-lg shadow">
+              <ul class="divide-y-2 divide-gray-100">
+                  <li class="p-3"><span class="font-medium">Contract address: </span> <br>{{ contractAddress }}</li>
+                  <li class="p-3"><span class="font-medium">Last Winner: </span>  <br>{{ lastWinner  }}</li>
+                  <li class="p-3"><span class="font-medium">Manager Address: </span> <br> {{ managerAddres }}</li>
+                  <li class="p-3"><span class="font-medium">Cantidad de Jugadores: <br>{{ cantPlayers }} </span></li>  
+              </ul>
+          </div>
         </div>
       </div>
   
